564efb085c6f234d5cc0f2e4461c7ed0cb20a5d6
doc ahrens <risk.analyzer@gmail.com>
2016-03-12 16:54:50 -0800

    Programming Exercise 2
    Program computes the inverse of the matrix returned by makeCacheMatrix.
    If the inverse has already been calculated, then the program retrieves
    the inverse from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makeCacheMatrix sets up the attributes and methods for
+## a matrix caching object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() {
+    x
+  }
+  setinv <- function(inv) {
+    m <<- inv
+  }
+  getinv <- function() {
+    m
+  }
+  
+  #    label     function
+  list(set    = set,
+       get    = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## if an inverted matrix already exists, cacheSolve returns it
+## if no inverted matrix exists, cacheSolve inverts it and returns it
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  
+  m
 }
+
+## driver code to test functions
+
+orig_mat <- matrix(rnorm(25), 5, 5)
+
+a <- makeCacheMatrix(orig_mat)
+
+cacheSolve(a)
